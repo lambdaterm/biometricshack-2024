479d154327ff146dcd5f8844ad41a891f342697c
Aleksey Lenko <alenko@IDScan.net>
2024-10-11 13:35:01 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -16,19 +16,19 @@ def move_files_and_remove_folder(source_folder, destination_folder):
     source.rmdir()
 
 
-# clone_repo('https://github.com/foivospar/Arc2Face.git', "./arc2face")
+subprocess.check_call(['git', 'clone', 'https://github.com/foivospar/Arc2Face.git', "./arc2face"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "torch==2.4.0", "torchvision==0.19.0", "torchaudio==2.4.0", "--index-url", "https://download.pytorch.org/whl/cu124"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "huggingface_hub==0.24.0", "opencv-python"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "./arc2face/requirements.txt"])
 
-# subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "./arc2face/requirements.txt"])
-# subprocess.check_call([sys.executable, "-m", "pip", "uninstall", "-y", "huggingface_hub"])
-# subprocess.check_call([sys.executable, "-m", "pip", "install", "huggingface_hub==0.23.0"])
 
 from huggingface_hub import hf_hub_download
 
-# hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="arc2face/config.json", local_dir="./models")
-# hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="arc2face/diffusion_pytorch_model.safetensors", local_dir="./models")
-# hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="encoder/config.json", local_dir="./models")
-# hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="encoder/pytorch_model.bin", local_dir="./models")
-# hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="arcface.onnx", local_dir="./models/antelopev2")
+hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="arc2face/config.json", local_dir="./models")
+hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="arc2face/diffusion_pytorch_model.safetensors", local_dir="./models")
+hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="encoder/config.json", local_dir="./models")
+hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="encoder/pytorch_model.bin", local_dir="./models")
+hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="arcface.onnx", local_dir="./models/antelopev2")
 
 from insightface.utils.storage import download_onnx
 
